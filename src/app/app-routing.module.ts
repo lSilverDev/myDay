f2ccbce987a70d42f376079dc5c904f04cadacab
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-04-24 18:37:39 -0300

    onDate - project changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { ProfileComponent } from './views/profile/profile.component';
-import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent,
-    pathMatch: 'full'
-  },
-  {
-    path: 'dashboard',
     component: DashboardComponent,
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
+    pathMatch: 'full'
   }
 ];
 
